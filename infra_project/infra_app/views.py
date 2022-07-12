bc0eae57bdb384b60ce76ec8185a4bb22730892d
Ян Мелехин <vladmakaroff.makarov@yandex.ru>
2022-07-12 18:17:53 +0500

    dsdfds

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -1,9 +1,10 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse('У меня получилось!.')
+    return HttpResponse('У меня получилось!')
 
 
 def second_page(request):
-    return HttpResponse(request, 'А это вторая страница.')
+    return render(request, 'А это вторая страница')
