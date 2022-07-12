ef617334023704d7b55c47b02acb5cccfd25840f
Ян Мелехин <vladmakaroff.makarov@yandex.ru>
2022-07-12 19:31:04 +0500

    AAAAAAААААА0000

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -1,16 +1,11 @@
 from django.http import HttpResponse
-from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'index.html')
+    html = 'У меня получилось!'
+    return HttpResponse(html)
 
 
 def second_page(request):
-    if request.method == "POST":
-        username = request.POST.get('username')
-        if username:
-            return HttpResponse('А это вторая страница')
-        else:
-            return HttpResponse('please, enter your username')
-    return render(request, 'second_page.html')
+    html = 'А это вторая страница'
+    return HttpResponse(html)
